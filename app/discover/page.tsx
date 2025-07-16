04c0e601bedde92052bca32038c6738dd4df83cb
ssklin <shengkuo.lin@outlook.com>
2025-07-16 12:22:52 -0700

    feat: add discover section 3

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,10 +1,13 @@
+import DiscoverSec3 from "./components/discover3";
+
 export default function DiscoverPage() {
   return (
-    <div className="min-h-screen pt-20">
-      <h1 className="text-4xl font-bold text-center">Discover FiPet</h1>
-      <p className="text-center mt-4">
-        Explore our features and learn how we can help you manage your finances.
-      </p>
-    </div>
+    <>
+      <div className="min-h-screen pt-20">
+        <h1 className="text-4xl font-bold text-center">Discover FiPet</h1>
+        <p className="text-center mt-4">Explore our features and learn how we can help you manage your finances.</p>
+      </div>
+      <DiscoverSec3></DiscoverSec3>
+    </>
   );
 }
