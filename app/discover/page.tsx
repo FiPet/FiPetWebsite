d1e245e533fb0acf76fe85bd811eeb72644cba3b
Jitesh-Gadage16 <gadagejitesh16@gmail.com>
2025-07-16 15:16:16 -0400

    done with discover section two

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,8 +1,15 @@
+import React from 'react';
+import "../globals.css";
+
+import DiscoverSection2 from '@/components/discoversection2';
+
 export default function DiscoverPage() {
   return (
     <div className="min-h-screen pt-20">
       <h1 className="text-4xl font-bold text-center">Discover FiPet</h1>
       <p className="text-center mt-4">Explore our features and learn how we can help you manage your finances.</p>
+
+      <DiscoverSection2 />
     </div>
   );
 }
