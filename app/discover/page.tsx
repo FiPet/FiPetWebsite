9d9452f1f2141f1614dc9057f699f8eac54558ca
ssklin <shengkuo.lin@outlook.com>
2025-07-24 16:07:40 -0700

    style(discover): Apply white background color to mobile view but retain desktop view

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -6,7 +6,7 @@ import DiscoverSection3 from "@/components/discover/Discoversection3";
 
 export default function DiscoverPage() {
   return (
-    <div className=" w-full bg-[#FFF8EC]  ">
+    <div className="w-full bg-white sm:bg-[#FFF8EC]">
       <DiscoverSection1 />
       <DiscoverSection2 />
       <DiscoverSection3 />
